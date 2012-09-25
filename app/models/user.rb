4fe06e8f6c997e858caa1be4062e23ab9245c05d
Raul Garcia <raul.ivan.82@hotmail.com>
2012-09-25 15:41:46 -0700

    Create the user model, add several records to the seeds file and add the attr_accesible to the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   # attr_accessible :title, :body
+  
+attr_accessible  :first_name, :last_name, :username, :password, :email, :is_admin
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+
+user = User.create([
+  { first_name: 'Raul Ivan', last_name:"Garcia Valdivia",username:"reversible_82",password:"kamikaze",email:"raul.ivan.82@hotmail.com",is_admin:true },
+  { first_name: 'Mario', last_name:"Zarate",username:"zarate_82",password:"password",email:"zarate82@hotmail.com",is_admin:false},
+   { first_name: 'Jose', last_name:"Morris",username:"morris_82",password:"password",email:"morris.82@hotmail.com",is_admin:false },
+    { first_name: 'Adriana', last_name:"Utomic",username:"utonic_82",password:"password",email:"utomic.82@hotmail.com",is_admin:false},
+     { first_name: 'Karla', last_name:"Kwan",username:"kwan_82",password:"password",email:"kwan.82@hotmail.com",is_admin: false}
+])
