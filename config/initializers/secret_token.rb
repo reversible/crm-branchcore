70c505c89a10e86cc1be6c460b4f108b2d4de8eb
Raul Garcia <raul.ivan.82@hotmail.com>
2012-09-25 14:58:00 -0700

    Added the files for the project, also created the User model and migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrmBranchcore::Application.config.secret_token = '162936546fbac24b05238fc042748fa975fbf6875265a38aeefcc68f1c7ea37dd2d238400f795d650ede494bb8e1a7e0543fe885435519b8923fb1359493ce8b'
