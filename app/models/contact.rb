80ce6ba4a1dd6078044af1d3a3d659525d4f98c0
Ivan Garcia <reversible@localhost.localdomain>
2012-10-04 08:29:50 -0700

    -added validation for user and contact module -added the error style

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,10 +3,23 @@ class Contact < ActiveRecord::Base
   
   
   belongs_to :user
+  has_many :tasks
+  
+  
   self.per_page = 10;
 
   
-  attr_accessible :first_name, :last_name, :address, :status, :email,:user ,:user_id
+  attr_accessible :first_name, :last_name, :address, :status, :email,:email_confirmation, :user ,:user_id
+
+EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i  
+
+  
+validates :first_name, :presence=>true, :length => { :maximum => 40 }
+validates :last_name, :presence=>true, :length => { :maximum => 40 }
+validates :address, :presence=>true, :length => {:minimum=>15, :maximum => 1000 }
+validates :email,:presence=>true, :uniqueness => true, :format => EMAIL_REGEX, :confirmation => true,:length => { :maximum => 100 }  
+
+
 
  def name
     "#{first_name} #{last_name}"
