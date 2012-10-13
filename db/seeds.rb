d6ee08177de26c2ce64276d4e668caf4590a6a68
Ivan Garcia <raul.ivan.82@hotmail.com>
2012-10-13 15:42:49 -0700

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,46 +32,39 @@ puts "Success: Contacts data loaded"
 
 
 BusinessType.delete_all
-50.times do
+
 business_type = BusinessType.create([
   { sector: 'Information Technology'},
   { sector: 'Bio-Technology'},
   { sector: 'Administrative'},
 ])
-end
 puts "Success: Business types data loaded"
 
 
 Account.delete_all
-50.times do
 accounts = Account.create([
   { name: 'Nike', address: "some place in NYC", description:"Great shoes company", account_type: "enterprice", email: "rg@rdedeg.com", user: User.last , business_type: BusinessType.last  },
   { name: 'CocaCOla', address: "some place in Minesota", description:"Great Soda company", account_type: "small", email: "rgc@rdedeg.com",  user: User.last , business_type: BusinessType.last  },
   { name: '37 Signals', address: "some place in Chicago", description:"Web application", account_type: "huge", email: "rgr@rdedeg.com",  user: User.last , business_type: BusinessType.last },
 ])
-end
 puts "Success: Accounts data loaded"
 
 Opportunity.delete_all
-50.times do
 opportunities = Opportunity.create([
   { amount: 10000.00, status: "ACTIVE", description:"Talks with nike in order to design the new product", user: User.last, account: Account.last  },
     { amount: 500000.00, status: "INACTIVE", description:"Talks with two in a half man", user: User.last, account: Account.last  }
  
 ])
-end
 puts "Success: Opportunities data loaded"
 
 
 Task.delete_all
-50.times do
 task = Task.create([
 { subject: "Deliver the Project Plan", status: "ACTIVE", start_date: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M") , limit_date: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"), end_date: DateTime.strptime("09/20/2009 8:00", "%m/%d/%Y %H:%M"), priority: 1 , user: User.last, account: Account.last, contact: Contact.last },
 { subject: "Meeting with the BR", status: "ACTIVE", start_date: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"), limit_date: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"), end_date: DateTime.strptime("09/20/2009 8:00", "%m/%d/%Y %H:%M"), priority: 2 ,  user: User.last, account: Account.last, contact: Contact.last },
 { subject: "Tea time with british airlines", status: "ACTIVE", start_date: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"), limit_date: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"), end_date: DateTime.strptime("09/20/2009 8:00", "%m/%d/%Y %H:%M"), priority: 3 , user: User.last, account: Account.last, contact: Contact.last },    
  
 ])
-end
 puts "Success: Task data loaded"
 
 
